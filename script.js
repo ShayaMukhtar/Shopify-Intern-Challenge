9967f112fe92c2bfb0845e7957054aa21472af03
ShayaMukhtar <Shaya.619@hotmail.com>
2022-01-18 18:46:10 -0500

    Updates to Shopify Intern Challenge

--- script.js
+++ script.js
@@ -1,44 +1,51 @@
-function fetchdata(){
-fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=g9b7LSFuaR7VYn6xzqqtj6EyuEAd8wmVeesm01Mm')
-.then(response =>{
-if (!response.ok) {
-    throw Error("ERROR")
-}    
-return response.json()
-.then(data => {
-    const html = data.photos.map(user => {
+/*
+    name: fetchCuriousityRoverPictures
+    description: this function gets ...
+*/
 
-        return ` 
-        <div class="card">
-        <img src= "${user.img_src}" alt="Mars Photo"/>
-        <div class="card-body">
-        <p>Date: ${user.earth_date}</p>
-        <p>Rover: ${user.rover.status}</p>
-        <br>
-        <button onclick="btn1(this)" id="button">Like</button>
-        </div>
-        </div>`
+function fetchCuriousityRoverPictures(){
+    fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=g9b7LSFuaR7VYn6xzqqtj6EyuEAd8wmVeesm01Mm')
+    .then(response =>{
+        if (!response.ok) {
+            throw Error("ERROR")
+        }    
+        return response.json()
+        .then(data => {
+            const html = data.photos.map(user => {
+                return ` 
+                    <div class="card">
+                        <img src= "${user.img_src}" alt="Mars Photo"/>
+                        <div class="card-body">
+                            <p>Date: ${user.earth_date}</p>
+                            <p>Rover: ${user.rover.status}</p>
+                            <br>
+                            <button onclick="toggleLikeButton(this)" id="button">Like</button>
+                        </div>
+                    </div>`
+                }).join ("");
 
-    }).join ("");
-    let space = document.querySelector('#space').insertAdjacentHTML("afterbegin", html)
-    let space1 = document.querySelector('#space1')
-    let pic1 = document.querySelector('#pic1', html)
-    var myobj = document.querySelector("#imageLoadSpinner");
-myobj.remove();
-});
-
-});
-
-}
-fetchdata();
-function btn1(info) {
-   if(info.innerHTML == "Like") {
-       info.innerHTML = "❤️"
-       
-   } 
-   else if(info.innerHTML == "❤️"){
-       info.innerHTML = "Like"
-   }
-   
+            document.querySelector('#space').insertAdjacentHTML("afterbegin", html)
+            document.querySelector('#space1')
+            document.querySelector('#pic1', html)
+            
+            var myobj = document.querySelector("#imageLoadSpinner");
+            myobj.remove();
+        });
+    });
 }
 
+/*
+    name: toggleLikeButton
+    description: this function gets ...
+*/
+    
+function toggleLikeButton(info) {
+    if(info.innerHTML == "Like") {
+        info.innerHTML = "❤️"
+    } 
+    else if(info.innerHTML == "❤️"){
+        info.innerHTML = "Like"
+    } 
+}
+
+fetchCuriousityRoverPictures();
